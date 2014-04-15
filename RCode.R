e7c7a8c86e58db5e851beed2e1a250350505c212
jrzelling <jryanzelling@gmail.com>
2014-04-15 02:23:06 -0500

    Added code

--- RCode.R
+++ RCode.R
@@ -1,2 +1,19 @@
 # First Line
+# setwd("~/Downloads/pollutionmonitordata/")
+
+pollutantmean <- function (
+        directory = "~/Downloads/pollutionmonitordata/specdata/", 
+        pollutant = "pollutant", 
+        id = 101) {
+        
+        setwd(directory)
+        
+        filenames<-as.character( list.files() )
+        directory<-paste( directory, filenames, sep="" )
+        
+        files <- read.csv(directory[id])
+        
+        return( head(files) )
+}
+
 
