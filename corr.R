545d76cc2e0b00de86b764a20fba0fc8b84954d4
jrzelling <jryanzelling@gmail.com>
2014-04-17 04:59:37 -0500

    testing, not working

--- corr.R
+++ corr.R
@@ -14,7 +14,7 @@ corr <- function(
         filepaths <- paste( directory, filenames, sep="" )
         #initialize complete cases
         filecount <- length( list.files() )
-        cases <- complete(directory, id = 1:filecount)
+        cases <- complete("specdata", id = 1:filecount)
         
         valid <- cases[,3]>=threshold
         cases[,3][valid]
