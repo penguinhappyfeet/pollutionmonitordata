b099ee1b8c12891346457b3e6d5cb6f4505316f4
jrzelling <jryanzelling@gmail.com>
2014-04-17 02:46:41 -0500

    added test

--- RCode.R
+++ RCode.R
@@ -2,15 +2,23 @@
 setwd("~/Downloads/pollutionmonitordata/")
 
 pollutantmean <- function (
-        directory = "~/Downloads/pollutionmonitordata/specdata/", 
+        directory = "specdata", 
         pollutant = "sulfate", 
         id = 101) {
 
-        setwd(directory)
-        filenames<-as.character( list.files() )
-        directory<-paste( directory, filenames, sep="" )
+        root <- "~/Downloads/pollutionmonitordata/"
+        directory <- paste( root, directory, "/", sep="" )
+        setwd( directory )
+        filenames <- as.character( list.files() )
+        #print( head(filenames) )
+        #print 
         
-        loadfile <- read.csv( directory[id] )
+        filepaths <- paste( directory, filenames, sep="" )
+        #print( head(filepaths) )
+        #print( filepaths[1] )
+        
+        loadfile <- c( read.csv( filepaths[ id ] ) )
+        #print( filepaths[ filenames[1] ] )
         
         result <- loadfile[,pollutant]
         
@@ -19,5 +27,5 @@ pollutantmean <- function (
         return( mean( valid ) )
 }
 
-a <- pollutantmean( pollutant = "sulfate", id = 101 )
+a <- pollutantmean( pollutant = "sulfate", id = 1 )
 a
---
+++ tests.R
@@ -0,0 +1,22 @@
+# First Line
+
+id = 1:10
+pollutant = "sulfate"
+directory = "~/Downloads/pollutionmonitordata/specdata/"
+setwd("~/Downloads/pollutionmonitordata/specdata/")
+filemeans <- c()
+filenames <- as.character( list.files() )
+filepaths <- paste( directory, filenames, sep="" )
+
+for(i in id) {
+        loadfile <- read.csv( filepaths[i] )
+        result <- loadfile[,pollutant]
+        valid <- result[ !is.na( result ) ]
+        filemeans <- c(filemeans, valid )
+}
+
+
+loadfile
+result
+valid
+mean(filemeans)
