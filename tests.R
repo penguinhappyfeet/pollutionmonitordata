c2c25bc0e95eb7c705e8a1195b6df5b3261a7ed4
jrzelling <jryanzelling@gmail.com>
2014-04-17 02:59:02 -0500

    working function

--- tests.R
+++ tests.R
@@ -4,6 +4,7 @@ id = 1:10
 pollutant = "sulfate"
 directory = "~/Downloads/pollutionmonitordata/specdata/"
 setwd("~/Downloads/pollutionmonitordata/specdata/")
+
 filemeans <- c()
 filenames <- as.character( list.files() )
 filepaths <- paste( directory, filenames, sep="" )
